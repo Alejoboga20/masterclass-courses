f0cc69eccdaf77d41c0dd2a201147f9d46b10dc3
Alejo Boga <alejoboga20@gmail.com>
2022-04-02 08:52:58 -0300

    Add global style

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import * as ReactDOMClient from 'react-dom/client';
 import { App } from './App';
+import './styles/index.css';
 
 const container = document.getElementById('root') as Element;
 const root = ReactDOMClient.createRoot(container);
