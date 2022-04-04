8abe799157a2938f6b3d6c405bf5b862e8cc8014
Alejo Boga <alejoboga20@gmail.com>
2022-04-04 18:00:35 -0300

    Adding text for hover in card

--- src/components/InstructorCard/InstructorCard.tsx
+++ src/components/InstructorCard/InstructorCard.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useFavorite } from 'hooks/useFavorite';
 import Heart from 'assets/heart.png';
 import styles from './InstructorCard.module.css';
@@ -10,16 +11,33 @@ export const InstructorCard = ({
 	title,
 }: IntructorCardProps) => {
 	const { handleOnClick, isFavorite } = useFavorite(favorite, id);
+	const [isMouseIn, setIsMouseIn] = useState(false);
+
+	const onMouseEnter = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => setIsMouseIn(true);
+	const onMouseLeaver = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => setIsMouseIn(false);
 
 	return (
-		<div className={styles.instructor__container} onClick={handleOnClick}>
+		<div
+			className={styles.instructor__container}
+			onClick={handleOnClick}
+			onMouseEnter={onMouseEnter}
+			onMouseLeave={onMouseLeaver}
+		>
 			<div className={styles.instructor__image}>
 				<img src={instructor_image_url} alt={instructor_name} />
 			</div>
 
 			<div className={styles.instructor__text}>
-				<h3 className={styles.instructor__name}>{instructor_name}</h3>
-				<p className={styles.instructor__title}>{title}</p>
+				<div>
+					<h3 className={styles.instructor__name}>{instructor_name}</h3>
+					<p className={styles.instructor__title}>{title}</p>
+				</div>
+
+				{isMouseIn && (
+					<small className={styles.instructor__alert}>
+						{isFavorite ? 'Remove from favorites' : 'Add to favorites'}
+					</small>
+				)}
 			</div>
 
 			{isFavorite && (
