f66db55fba4d1086ef384ae371b1998e9796272b
Alejo Boga <alejoboga20@gmail.com>
2022-04-06 10:30:38 -0300

    Adding text in constants

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,24 +1,24 @@
-import { routePaths } from 'constants/common';
+import { routePaths, text } from 'constants/common';
 import { NavLink } from 'react-router-dom';
 import styles from './Header.module.css';
 
 export const Header = () => {
 	return (
 		<nav className={styles.header}>
-			<h1 className={styles.header__title}>Instructors</h1>
+			<h1 className={styles.header__title}>{text.headerTitle}</h1>
 
 			<div className={styles.header__navigation}>
 				<NavLink
 					style={(navData) => (navData.isActive ? { color: '#ff1a66', fontWeight: 'bold' } : {})}
 					to={routePaths.courses}
 				>
-					All
+					{text.coursesLink}
 				</NavLink>
 				<NavLink
 					style={(navData) => (navData.isActive ? { color: '#ff1a66', fontWeight: 'bold' } : {})}
 					to={routePaths.favorites}
 				>
-					Favorites
+					{text.favoritesLink}
 				</NavLink>
 			</div>
 			<hr />
