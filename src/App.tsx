aac648f1911e556c2d5c3f024c75005764f180b0
Alejo Boga <alejoboga20@gmail.com>
2022-04-07 11:32:33 -0300

    Add pretierrc file

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,16 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { routes } from 'router';
 
 export const App = () => {
-	return (
-		<CoursesProvider>
-			<BrowserRouter>
-				<Routes>
-					{React.Children.toArray(
-						routes.map(({ path, Element }) => <Route path={path} element={<Element />} />)
-					)}
-					<Route path='*' element={<Navigate to='/courses' replace />} />
-				</Routes>
-			</BrowserRouter>
-		</CoursesProvider>
-	);
+  return (
+    <CoursesProvider>
+      <BrowserRouter>
+        <Routes>
+          {React.Children.toArray(
+            routes.map(({ path, Element }) => <Route path={path} element={<Element />} />)
+          )}
+          <Route path="*" element={<Navigate to="/courses" replace />} />
+        </Routes>
+      </BrowserRouter>
+    </CoursesProvider>
+  );
 };
